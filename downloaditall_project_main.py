0558017b5b151c44acea45f7e9519e528b5045b3
Sam <scadet1@gmail.com>
2021-09-24 00:30:38 -0400

    Third Commit, download button works but no audio beyond 720p videos

--- downloaditall_project_main.py
+++ downloaditall_project_main.py
@@ -11,7 +11,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui
-
+import re
 
 # from downloaditall_project_main import browseFiles, clickDownload, provideURL, fileProgress
 
@@ -27,21 +27,20 @@ class Window(QMainWindow, Ui_MainWindow):
         self.videoTitle
         self.selectVideoQualityBox
         self.downloadButton.clicked.connect(self.clickDownload)
-        self.downloadButton.clicked.connect(self.fileProgress)
+        # self.downloadButton.clicked.connect(self.fileProgress)
         self.progressBar.setMaximum(100)
         self.URLBar
         self.checkURL.clicked.connect(self.provideURL)
 
     def browseFiles(self):
-        global filename
+
         # path = os.path.join('', )
-        filename = QFileDialog.getExistingDirectory(
+        self.filename = QFileDialog.getExistingDirectory(
             self, 'Save File')
-        if filename:
-            self.destinationBar.setText(str(filename))
+        if self.filename:
+            self.destinationBar.setText(str(self.filename))
 
     def provideURL(self, audio_only=False):
-        global yt
         self.yt = YouTube(self.URLBar.text())
         if self.yt:
             self.streams = self.yt.streams
@@ -55,22 +54,22 @@ class Window(QMainWindow, Ui_MainWindow):
             pixmap.scaled(400, 225, Qt.KeepAspectRatio,
                           Qt.FastTransformation)
 
-            for stream in self.streams.all():
+            for stream in self.streams:
                 self.streams.order_by('resolution')
                 self.selectVideoQualityBox.addItem(str(stream))
 
     def clickDownload(self):
-        self.yt = YouTube(self.URLBar.text())
-        self.streams = self.yt.streams.get_highest_resolution()
-        itag = self.selectVideoQualityBox.itemData(
-            self.selectVideoQualityBox.currentIndex())
-        self.yt.register_on_progress_callback(self.fileProgress)
-        self.streams.download(output_path=filename, itag=itag)
+        selection = (self.selectVideoQualityBox.currentText())
+        itag = re.findall(r'itag="(.*?)"', selection)[0]
+        print(itag)
+        # self.yt.register_on_progress_callback(self.fileProgress)
+        file = self.streams.get_by_itag(itag)
+        file.download(output_path=self.filename)
 
-    def fileProgress(self, totalSize, stream=None, chunk=None, remaining=None):
-        fileSize = stream.filesize
-        self.progressBar.setValue(100 * (fileSize - remaining)) / fileSize
-        # QApplication.processEvents()
+    # def fileProgress(self, totalSize, stream=None, chunk=None, remaining=None):
+    #     fileSize = self.stream.filesize
+    #     self.progressBar.setValue(100 * (fileSize - remaining)) / fileSize
+    #     QApplication.processEvents()
 
 
 if __name__ == '__main__':
